986473658947bb6e8016055b8d20027bc80f6760
Kolya <code@kachur.me>
2022-06-10 21:57:24 -0400

    Initial happiness commit.

---
+++ happiness.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import argparse
+import datetime
+import pathlib
+import random
+import time
+
+EMPTY_GREETING = ""
+MAX_DELAY_SECONDS = 7
+
+
+def get_args():
+    """Return command-line arguments."""
+    parser = argparse.ArgumentParser(description="Wishes you happy days.")
+    parser.add_argument(
+        "-d", "--day", default=f"{datetime.date.today():%A}",
+        help="which day to wish you happiness for"
+    )
+    parser.add_argument(
+        "-g", "--greetings", type=pathlib.Path,
+        help="text file of additional greetings"
+    )
+    return parser.parse_args()
+
+
+def get_random_greeting(config):
+    """Get a uniformly random greeting from a text config file."""
+    if not config:
+        return EMPTY_GREETING
+    greetings = config.read_text()
+    greetings.append(EMPTY_GREETING)
+    return random.choice(greetings)
+
+
+def main():
+    """Wish the user happy days."""
+    args = get_args()
+    happiness = f"Happy {args.day}!"
+    greeting = get_random_greeting(args.greetings)
+    if greeting != EMPTY_GREETING:
+        happiness = f"{greeting} {happiness}"
+    delay_seconds = random.randrange(MAX_DELAY_SECONDS)
+    time.sleep(delay_seconds)
+    print(happiness)
+
+
+if __name__ == "__main__":
+    main()
