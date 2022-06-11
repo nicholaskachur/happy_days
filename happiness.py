413531c2853de4290d1f3567d7ba26ecc88f2ee0
Kolya <code@kachur.me>
2022-06-11 08:22:34 -0400

    Add special days and clarify language.

--- happiness.py
+++ happiness.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import argparse
+import collections
+import csv
 import datetime
 import pathlib
 import random
 import time
 
-EMPTY_GREETING = ""
+EMPTY_SALUTATION = ""
 MAX_DELAY_SECONDS = 3
 
 
@@ -13,32 +15,49 @@ def get_args():
     """Return command-line arguments."""
     parser = argparse.ArgumentParser(description="Wishes you happy days.")
     parser.add_argument(
-        "-d", "--day", default=f"{datetime.date.today():%A}",
-        help="which day to wish you happiness for"
+        "-d", "--days", type=pathlib.Path,
+        help="csv file of special days",
     )
     parser.add_argument(
-        "-g", "--greetings", type=pathlib.Path,
-        help="text file of additional greetings"
+        "-s", "--salutations", type=pathlib.Path,
+        help="text file of salutations to add at beginning of greeting",
     )
     return parser.parse_args()
 
 
-def get_random_greeting(config):
-    """Get a uniformly random greeting from a text config file."""
-    greetings = [EMPTY_GREETING]
-    if config:
-        with config.open("r") as f:
-            greetings.extend(line.strip() for line in f)
-    return random.choice(greetings)
+def get_happiness(days_config):
+    """Return a "Happy <day>!" string allowing for special days."""
+    today = datetime.date.today()
+    if days_config:
+        with days_config.open("r", newline="") as f:
+            days = collections.defaultdict(list)
+            reader = csv.DictReader(f)
+            for line in reader:
+                date = datetime.datetime.strptime(line["date"], "%Y-%m-%d").date()
+                days[date].append(line["text"])
+        try:
+            return " and ".join(days[today])
+        except KeyError:
+            pass
+    return f"Happy {today:%A}!"
+
+
+def get_random_salutation(salutations_config):
+    """Return a uniformly random salutation from a text config file."""
+    salutations = [EMPTY_SALUTATION]
+    if salutations_config:
+        with salutations_config.open("r") as f:
+            salutations.extend(line.strip() for line in f)
+    return random.choice(salutations)
 
 
 def main():
     """Wish the user happy days."""
     args = get_args()
-    happiness = f"Happy {args.day}!"
-    greeting = get_random_greeting(args.greetings)
-    if greeting != EMPTY_GREETING:
-        happiness = f"{greeting} {happiness}"
+    happiness = get_happiness(args.days)
+    salutation = get_random_salutation(args.salutations)
+    if salutation != EMPTY_SALUTATION:
+        happiness = f"{salutation} {happiness}"
     delay_seconds = random.randrange(MAX_DELAY_SECONDS)
     time.sleep(delay_seconds)
     print(happiness)
